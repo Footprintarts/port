f027178ffd2153e27e3f4efe5fbdcccdce0fe0fb
Footprintarts <mallyac17@gmail.com>
2025-01-27 13:03:01 +0300

    added project links

--- src/components/ProjectCardPro.jsx
+++ src/components/ProjectCardPro.jsx
@@ -26,37 +26,33 @@ const ProjectCard = ({ project }) => {
   };
 
   return (
-    <a href="/test-link">
+    <a href={project.link} target="_blank" rel="noopener noreferrer">
       <div
-        className="z-[999] card w-fit rounded-md hover:shadow-2xl hover:shadow-lime-500 duration-300 cursor-pointer transition-all p-4 relative"
+        className="card w-fit rounded-md hover:shadow-2xl hover:shadow-lime-500 duration-300 cursor-pointer transition-all p-4 relative z-[1]" // Ensure card has lower z-index than hover image
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
         {/* Main card content */}
-        <figure className="rounded-lg img-holder w-[334px] h-[250px] overflow-hidden">
+        <figure className="img-holder w-[334px] h-[250px] overflow-hidden rounded-lg relative">
           <img
             src={project.image}
             alt={project.title}
-            className="z-10 img-cover w-full h-full object-cover"
+            className="img-cover w-full h-full object-cover z-[1]"
           />
         </figure>
 
         <div className="card-content mt-4">
-          <span className="label-large card-subtitle text-sm text-zinc-500">
+          <span className="card-subtitle text-sm text-zinc-500">
             {project.type}
           </span>
 
-          <h3 className="title-large card-title text-xl font-bold">
-            {project.title}
-          </h3>
+          <h3 className="card-title text-xl font-bold">{project.title}</h3>
         </div>
-
-        <a href="#" className="state-layer absolute inset-0"></a>
 
         {/* Hover Image */}
         {isHovered && (
           <div
-            className="fixed top-0 left-0 pointer-events-none z-[999] transition-opacity duration-300 flex ease-out"
+            className="fixed top-0 left-0 pointer-events-none z-[9999]" // Ensure hover image is on top of everything
             style={{
               transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
             }}
@@ -64,7 +60,7 @@ const ProjectCard = ({ project }) => {
             <img
               src={project.cover1}
               alt={project.title}
-              className="z-[999] w-40 h-40 lg:w-[320px] lg:h-96 object-cover rounded-lg shadow-lg"
+              className="w-40 h-40 lg:w-[320px] lg:h-96 object-cover rounded-lg shadow-lg"
             />
           </div>
         )}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,36 +9,42 @@ const Projects = () => {
       title: "Pixstock - Stock Media App",
       type: "Web App",
       cover1: "/covers/cover1.gif",
+      link: "https://aboonefinance.pages.dev/",
     },
     {
       image: images.Project2,
       title: "Cook.io - Recipe App",
       type: "Web App",
       cover1: "/covers/cover2.gif",
+      link: "https://orangeweb.pages.dev/",
     },
     {
       image: images.Project3,
       title: "GitFinder - Github User Finder",
       type: "Web App",
       cover1: "/covers/cover3.gif",
+      link: "https://digitaldesigner.pages.dev/",
     },
     {
       image: images.Project4,
       title: "Weatherio - Live Weather App",
       type: "Web App",
       cover1: "/covers/cover4.gif",
+      link: "https://bonapex-website.pages.dev/",
     },
     {
       image: images.Project5,
       title: "Tvflix - Netflix Clone 2.0",
       type: "Web App",
       cover1: "/covers/cover5.gif",
+      link: "https://bondoru.pages.dev/",
     },
     {
       image: images.Project6,
       title: "Travelia - Travel Website",
       type: "Website",
       cover1: "/covers/cover6.gif",
+      link: "https://docs-clone.pages.dev",
     },
   ];
 
